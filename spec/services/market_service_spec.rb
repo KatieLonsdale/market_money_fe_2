d63a49da9fbf58a3aad1fe05adbbcff87aba16d8
Katie Lonsdale <katie952@gmail.com>
2023-05-25 13:20:57 -0600

    test: add test for all_markets

---
+++ spec/services/market_service_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe MarketService do
+  describe 'class methods' do
+    describe 'all_markets' do
+      it 'returns results for all markets in BE API' do
+        markets = MarketService.all_markets[:data]
+        
+        attributes = [:name, :street, :city, :county, :state, :zip, :lat, :lon, :vendor_count]
+        markets.each do |market|
+          expect(market).to have_key(:id)
+          attributes.each do |attribute|
+            expect(market[:attributes]).to have_key(attribute)
+          end
+        end
+      end
+    end
+  end
+end
