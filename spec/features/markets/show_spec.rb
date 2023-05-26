ef51bd840d61f9b1045256dea7cac129c67286a0
Katie Lonsdale <katie952@gmail.com>
2023-05-25 18:53:01 -0600

    test: that vendors of market are listed and link to show page

--- spec/features/markets/show_spec.rb
+++ spec/features/markets/show_spec.rb
@@ -11,11 +11,23 @@ RSpec.describe 'market show page' do
       expect(page).to have_content("Virginia")
       expect(page).to have_content("24521")
     end
-    xit 'displays a list of vendors at that market' do
+    it 'displays a list of vendors at that market', :vcr do
+      visit market_path(322474)
 
+      within("#vendors") do
+        expect(page).to have_content("Orange County Olive Oil")
+        expect(page).to have_content("The Vodka Vault")
+        expect(page).to have_content("The Fabric Patch")
+      end
     end
-    xit 'links each vendor name to its show page' do
+    it 'links each vendor name to its show page', :vcr do
+      visit market_path(322474)
 
+      within("#vendors") do
+        click_link "Orange County Olive Oil"
+      end
+
+      expect(current_path).to eq(vendor_path(55297))
     end
   end
 end
