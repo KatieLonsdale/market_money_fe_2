2db7e8fd5a8fa79b3b8476ff5a1e40e74aacb500
Katie Lonsdale <katie952@gmail.com>
2023-05-25 19:01:19 -0600

    feat: add initialize method

--- app/poros/market.rb
+++ app/poros/market.rb
@@ -2,11 +2,11 @@ class Market
   attr_reader :id, :name, :street, :city, :state, :zip
 
   def initialize(data)
-    @id = data.dig(:data, :id).to_i
-    @name = data.dig(:data, :attributes, :name)
-    @street = data.dig(:data, :attributes, :street)
-    @city = data.dig(:data, :attributes, :city)
-    @state = data.dig(:data, :attributes, :state)
-    @zip = data.dig(:data, :attributes, :zip)
+    @id = data[:id].to_i
+    @name = data.dig(:attributes, :name)
+    @street = data.dig(:attributes, :street)
+    @city = data.dig(:attributes, :city)
+    @state = data.dig(:attributes, :state)
+    @zip = data.dig(:attributes, :zip)
   end
 end
