bd2373ab996289274ef2329dc4bb655d0750fb2e
Katie Lonsdale <katie952@gmail.com>
2023-05-25 18:40:00 -0600

    feat: add all_vendors method

--- app/services/market_service.rb
+++ app/services/market_service.rb
@@ -7,6 +7,10 @@ class MarketService
     get_url("/api/v0/markets/#{id}")
   end
 
+  def self.all_vendors(id)
+    get_url("/api/v0/markets/#{id}/vendors")
+  end
+
   def self.get_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
