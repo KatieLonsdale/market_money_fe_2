a0187f3677835b7013ae135227b2382ae5382e11
Katie Lonsdale <katie952@gmail.com>
2023-05-25 18:13:34 -0600

    feat: add street and zip

--- app/poros/market.rb
+++ app/poros/market.rb
@@ -1,10 +1,12 @@
 class Market
-  attr_reader :id, :name, :city, :state
+  attr_reader :id, :name, :street, :city, :state, :zip
 
   def initialize(data)
     @id = data[:id].to_i
     @name = data.dig(:attributes, :name)
+    @street = data.dig(:attributes, :street)
     @city = data.dig(:attributes, :city)
     @state = data.dig(:attributes, :state)
+    @zip = data.dig(:attributes, :zip)
   end
 end
