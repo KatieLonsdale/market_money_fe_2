78bb6b508512aebe23adbe8122682237fde9855b
Katie Lonsdale <katie952@gmail.com>
2023-05-25 18:09:28 -0600

    test: that find market is returning correct market

--- spec/services/market_service_spec.rb
+++ spec/services/market_service_spec.rb
@@ -19,11 +19,15 @@ RSpec.describe MarketService do
       it 'returns the market with given id', :vcr do
         market = MarketService.find_market(322474)[:data]
         expect(market).to have_key(:id)
+        expect(market[:id]).to eq("322474")
 
         attributes = [:name, :street, :city, :county, :state, :zip, :lat, :lon, :vendor_count]
         attributes.each do |attribute|
           expect(market[:attributes]).to have_key(attribute)
         end
+
+        expect(market.dig(:attributes, :name)).to eq("2nd Street Farmers' Market")
+        expect(market.dig(:attributes, :street)).to eq("194 second street")
       end
     end
   end
